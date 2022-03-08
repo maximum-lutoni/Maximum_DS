6b1eed5039e3e81707caea8675de585b1a3ebcf3
maximum-lutoni <96006389+maximum-lutoni@users.noreply.github.com>
2022-03-08 13:14:00 +0300

    Добавил комментарии

--- project/Modules/news.py
+++ project/Modules/news.py
@@ -5,24 +5,22 @@ class NewsProcessor:
     country = "ru"
 
     def __init__(self):
-        pass
+        response: str
 
+    # получаем json ответ новостей
     def get_news(self):
         url = f"https://newsapi.org/v2/top-headlines?country={self.country}&apiKey={self.token}"
         res = requests.get(url)
         return res.json()["articles"][:10]
 
-
+    # разбираем полученный ответ 
     def format_response(self,news):
         res = ""
         for post in news:
             res += f"{post['title']}\n Источник: {post['url']}\n\n"
         return res
-
- #   def format_response_2(self,news):
- #       return "\n\n".join(f"{news["title"]}\n Источник: {news['url']}")
-
-
+    
+    # фунция для запуска
     def run(self):
         news = self.get_news()
         self.response = self.format_response(news)
