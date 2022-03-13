fca07321d4f5ffff564c6989d979358f56aaa7d2
maximum-lutoni <anton.lukashevich@maximumtest.ru>
2022-03-13 10:42:47 +0300

    Update course.py

--- project/Modules/course.py
+++ project/Modules/course.py
@@ -8,21 +8,20 @@ class CourseProcessor():
     url = "http://www.cbr.ru/scripts/XML_daily.asp?"
     response = "{0} рублей за 1 доллар \n {1} рублей за 1 евро \n {2} рублей за 10 юаней\n  {3} рублей за 1 фунт"
 
-    def __init__(self,id):
+    def __init__(self):
         today = datetime.today()
         today = today.strftime("%d/%m/%Y")
 
         payload = {"date_req": today}   
         response = requests.get(self.url, params=payload)
-        xml = BeautifulSoup(response.content, "lxml")
-        self.id = id
+        self.xml = BeautifulSoup(response.content, "lxml")
         
 
-    def _get_course(self):
+    def _get_course(self,id):
         return self.xml.find("valute",{'id': id }).value.text
 
     def _create_response(self):
-        self.response.format(self._get_course("R01235"),self._get_course("R01239"),self._get_course("R01375"),self._get_course("R01035"))
+        self.response = self.response.format(self._get_course("R01235"),self._get_course("R01239"),self._get_course("R01375"),self._get_course("R01035"))
 
     def run(self):
         self._create_response()
